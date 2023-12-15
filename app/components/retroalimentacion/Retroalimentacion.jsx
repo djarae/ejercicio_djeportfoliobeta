46fd5e5ba4e4db247cdb76290abf8efaabe913bb
djarae <djaraestivill@gmail.com>
2023-12-15 02:14:38 -0300

    9:Enviada data dinamica a api

--- app/components/retroalimentacion/Retroalimentacion.jsx
+++ app/components/retroalimentacion/Retroalimentacion.jsx
@@ -1,74 +1,50 @@
 "use client"
-
 import Espacios from '../espacios/Espacios.js'
 import React, { useState, useEffect,useMutation } from 'react';
 
-
-
-// import axios from 'axios';
-
-
 function Retroalimentacion() {
- console.log("Ejecutamos llamda api")
 
-    const [data,setData] = useState(null);
+    const [nombreHook,setNombreHook] = useState(" ");
+    const [consejoHook,setConsejoHook] = useState(" ");
+
+    function actuNombreHook(val){
+        setNombreHook(val);console.log(nombreHook)
+     }
+
+     function actuConsejoHook(val){
+        setConsejoHook(val);console.log(consejoHook)
+     }
+
+    
     useEffect(()=>{
         fetch("http://localhost:5000/")
         .then((response)=>response.json())
         .then((data)=> setData(data))
         .then(console.log("funciono recargar el coso"));
     },[]);
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    //use mutation
-    
-
+   
     function conectar (){
-        console.log("click")
-
         fetch("http://localhost:5000/retro", {
           method: "POST",
           mode: 'cors', 
           headers: {
             "Content-Type": "application/json",
           },
-      
           body: JSON.stringify({
-            user: { id: 1 },
-            title: 'El pepe2 : la venganza del pepe',
-            body: 'saquenme de latam',
+            nombre: nombreHook,
+            consejo: consejoHook,
           }),
         });
         return 0;
-
-
-
-
-
       }
-
-
-      
-
 
 
     
 
-    //fin use mut
+    
+      
+    
+     
 
 return (
 <div class="container row justify-content-center" >
@@ -76,31 +52,29 @@ return (
             Retroalimentacion
         </p>
         <div class="container col-6 rounded bg-primary  text-ligth  row justify-content-center" >
+             
               <h1 class="display-6 row justify-content-left text-light fs-4"   >
                   Favor de ingresar su nombre.
               </h1>
-              <input  id="inputNombre" class="form-control" ></input>
+              <input  id="inputNombre" class="form-control"
+                     onChange={e => actuNombreHook({val: e.target.value })}>
+              </input>
+              
               <Espacios cantidad="5"></Espacios>
+              
               <h1 class="display-6 row justify-content-left text-light fs-4"   >
-                  Favor de ingresar consejo.
+                  Favor de ingresar nombre.
               </h1>
-              <input  id="inputRetroalimentacion" class="form-control" ></input>
-              <button onClick={conectar}>CONECTAR</button>
+              <input  id="inputRetroalimentacion" class="form-control"
+                     onChange={e => actuConsejoHook({val: e.target.value })}>
+              </input>
+           
               <Espacios cantidad="5"></Espacios>
-          
-
-             
+                <button onClick={conectar}>CONECTAR</button>
               <Espacios  color="secondary" variant="contained" cantidad="5"></Espacios>
 
-              {/* <LlamadaApi></LlamadaApi> */}
-
-
-             
-    
-       
         </div>
   </div>
-
   );
 }
 export default Retroalimentacion;
